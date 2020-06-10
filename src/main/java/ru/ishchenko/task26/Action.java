3bb030d98bf2d8705fdc781d093c5b68d1610e7e
OldTreeFree <oldtreefree@gmail.com>
2020-06-10 18:11:07 +0300

    + add task 26

---
+++ src/main/java/ru/ishchenko/task26/Action.java
@@ -0,0 +1,7 @@
+package ru.ishchenko.task26;
+
+import java.util.Map;
+
+public interface Action {
+    boolean isUnique(Map<String, String> map);
+}
---
+++ src/main/java/ru/ishchenko/task26/Main.java
@@ -0,0 +1,33 @@
+package ru.ishchenko.task26;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        Action action = new Action() {
+            @Override
+            public boolean isUnique(Map<String, String> map) {
+                if (map.isEmpty())
+                    return true;
+                Set<String> values = new HashSet<>(map.values());
+                return map.size() == values.size();
+            }
+        };
+
+        //Проверка на пустую МАПу
+        Map<String, String> map = new HashMap<>();
+        System.out.println(action.isUnique(map));
+
+        map.put("Вася", "Пупкин");
+        map.put("Петя", "Пупкин");
+        map.put("Коля", "Сидоров");
+        System.out.println(action.isUnique(map));
+
+        map.remove("Петя");
+        System.out.println(action.isUnique(map));
+
+    }
+}
